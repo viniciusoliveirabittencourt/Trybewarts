63136ff82de92973c7da54a1802ec9ec63416ba1
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-07-28 01:57:36 -0300

    requisito 18

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function validAlert(event) {
 }
 
 document.getElementById('button-header').addEventListener('click', validAlert);
+
+const button = document.getElementById('submit-btn');
+button.disabled = 'true';
+
+let contador = 0;
+
+function habilitDesabilitButton() {
+  if (document.querySelector('#agreement').value === 'on') {
+    contador += 1;
+  }
+  if (contador % 2 !== 0) {
+    button.disabled = false;
+    button.style.backgroundColor = 'rgb(47 216 184)';
+  } else {
+    button.disabled = true;
+    button.style.backgroundColor = 'rgb(33 148 126)';
+  }
+}
+
+document.querySelector('#agreement').addEventListener('click', habilitDesabilitButton);
